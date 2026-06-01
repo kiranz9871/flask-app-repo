6d42de50e994e1b0608fc021b504c8756755551c
kiranz9871 <kiranzirpe9871@gmail.com>
2026-06-01 21:36:04 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "your app is working fine 🚀"
+    return "your app is working fine ,i love coding🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
